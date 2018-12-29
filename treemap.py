fbd998876840faccfe4f8bd0ed14644307d7137f
Snipta Mallick <39253021+sniptamallick@users.noreply.github.com>
2018-12-28 23:09:01 -0600

    Add files via upload
    Code

---
+++ treemap.py
@@ -0,0 +1,58 @@
+#import relevant packages 
+import pandas as pd
+import matplotlib 
+import matplotlib.pyplot as plt        
+import squarify 
+
+#read csv file 
+df = pd.read_csv("rankOrder.csv")
+
+#remove commas 
+df["Deaths"] = df["Deaths"].str.replace(",","").astype(float)
+
+#sort values in ascending order 
+df = df.sort_values(by="Deaths", ascending= False)
+
+#rename column 
+df.columns = df.columns.str.replace('Cause of death','Cause')
+
+#drop unnecessary rows and columns, specifically unnamed ones =
+#index refers to the row 
+df= df.drop( index= [12], columns= ['Unnamed: 0', 'Unnamed: 4', 'Unnamed: 3'])
+
+#specify color 
+cmap = matplotlib.cm.coolwarm
+
+#specify minimum and maximum values 
+mini = min(df["Deaths"])
+maxi = max(df["Deaths"])
+norm = matplotlib.colors.Normalize(vmin=mini, vmax=maxi)
+colors = [cmap(norm(value)) for value in df["Deaths"]]
+
+#plot using squarify 
+squarify.plot(sizes=df["Deaths"], label=df["Cause"], alpha = 1.0, color = colors)
+
+#remove axes on graph 
+plt.axis('off')
+
+#insert graph title 
+plt. title("Causes of Death in 2016", fontsize = 24)
+
+#show graph 
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
